aaf94e4a9e5fd704a9e2ea67bfac79674941f389
Alex Baier <abaier@uni-koblenz.de>
2017-05-02 22:35:16 +0200

    Added CORS.

--- curation-rest-api/src/main/java/de/unikoblenz/west/okbtoollabs/humavar/restapi/CurationEndpoint.java
+++ curation-rest-api/src/main/java/de/unikoblenz/west/okbtoollabs/humavar/restapi/CurationEndpoint.java
@@ -3,6 +3,10 @@ package de.unikoblenz.west.okbtoollabs.humavar.restapi;
 import spark.Spark;
 import spark.servlet.SparkApplication;
 
+import static spark.Spark.before;
+import static spark.Spark.get;
+import static spark.Spark.options;
+
 /**
  * okb-toollabs
  * Created by Alex on 02.05.2017 at 17:41.
@@ -11,13 +15,43 @@ public class CurationEndpoint implements SparkApplication {
 
     @Override
     public void init() {
-        Spark.get("/details/:item_id/:property_id", (req, res) -> "not implemented");
+        // TODO: not secure, change parameters somehow.
+        enableCORS("*", "*", "*");
 
-        Spark.get("/details/:item_id", (req, res) -> "not implemented");
+        get("/details/:item_id/:property_id", (req, res) -> "not implemented");
 
-        Spark.get("/summary/:item_id/:property_id", (req, res) -> "not implemented");
+        get("/details/:item_id", (req, res) -> "not implemented");
 
-        Spark.get("/summary/:item_id", (req, res) -> "not implemented");
+        get("/summary/:item_id/:property_id", (req, res) -> "not implemented");
 
+        get("/summary/:item_id", (req, res) -> "not implemented");
+
+    }
+
+    // Copied from: http://sparkjava.com/tutorials/cors
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
+        });
     }
 }
